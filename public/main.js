928900df2f4c47fc41b48f029510464e11558213
antonwy <anton.wyrowski@gmail.com>
2019-02-05 15:39:07 +0100

    added socket.io

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 const socket = io();
 
-socket.on('connect', () => {
+socket.on('connect', function() {
     console.log("Connected to server!");
 })
 
-socket.on('disconnect', () => {
+socket.on('disconnect', function() {
     console.log("Disconnected from server!");
 })
